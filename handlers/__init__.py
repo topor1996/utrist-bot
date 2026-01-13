72debec27cd6b0f505be687fdbaa0c2bfc4a8c08
t1 <andrey.toporovsky@mail.ru>
2026-01-13 22:00:26 +0300

    Fix imports: add process_appointment and process_question

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,8 +1,8 @@
 from .start import start_handler, main_menu_handler
 from .services import services_handler, legal_entities_handler, entrepreneurs_handler, individuals_handler
-from .appointment import appointment_handler, appointment_callback_handler
-from .question import question_handler
-from .admin import admin_handler, admin_callback_handler
+from .appointment import appointment_handler, appointment_callback_handler, process_appointment
+from .question import question_handler, process_question
+from .admin import admin_handler, admin_commands_handler, admin_callback_handler
 from .contacts import contacts_handler, about_handler
 
 __all__ = [
@@ -14,8 +14,11 @@ __all__ = [
     'individuals_handler',
     'appointment_handler',
     'appointment_callback_handler',
+    'process_appointment',
     'question_handler',
+    'process_question',
     'admin_handler',
+    'admin_commands_handler',
     'admin_callback_handler',
     'contacts_handler',
     'about_handler',
